f442ca4a9cdb11871fd59b34bee4261401e36308
Lê Thanh Nhã <tnhalk@maytinhdian.com>
2023-12-24 08:45:16 +0700

    Finish CRUD

--- laravel-api-sanctum/routes/api.php
+++ laravel-api-sanctum/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//public routes
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/products', [ProductController::class,'index']);
+Route::get('/products/{id}', [ProductController::class,'show']);
+Route::get('/products/search/{name}', [ProductController::class,'search']);
+
+//Protected routes-Only authenticated users can have access to protected routes//
+Route::group(['middleware' => ['auth:sanctum']], function () {
+     Route::post('/products',[ProductController::class,'store']);
+     Route::put('/products/{id}',[ProductController::class,'update']);
+     Route::delete('/products/{id}',[ProductController::class,'delete']);
+     Route::post('/logout',[AuthController::class,'logout']);
+
+
+ });
+
+
